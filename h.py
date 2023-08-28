fb85e55ecd84f5b1b7d2cae3a44353b6218a3063
jkamilo123 <142014545+jkamilo123@users.noreply.github.com>
2023-08-27 20:23:45 -0500

    Update h.py

--- h.py
+++ h.py
@@ -1,10 +1,26 @@
-import os
 import time
 
+def main():
+    horas = int(input("Ingrese las horas: "))
+    minutos = int(input("Ingrese los minutos: "))
+    segundos = int(input("Ingrese los segundos: "))
+    
+    tiempo_limite = horas * 3600 + minutos * 60 + segundos
+    
+    print("\nCronómetro iniciado:")
+    tiempo_restante = tiempo_limite
+    
+    while tiempo_restante > 0:
+        horas_restantes = tiempo_restante // 3600
+        minutos_restantes = (tiempo_restante % 3600) // 60
+        segundos_restantes = tiempo_restante % 60
+        
+        print(f"{horas_restantes:02d}:{minutos_restantes:02d}:{segundos_restantes:02d}", end="\r")
+        time.sleep(1)
+        
+        tiempo_restante -= 1
+    
+    print("\nTiempo límite alcanzado.")
 
-for hora in range(24):
-    for minuto in range(60):
-        for segundo in range(60):
-            os.system('cls')
-            print(f'{hora}:{minuto}:{segundo}')
-            time.sleep(1)
+if __name__ == "__main__":
+    main()
